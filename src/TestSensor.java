b9a01da5664fcf782e5a0570f2448b3040c431c4
Ariana <arianalima@github.com>
2020-11-14 16:49:55 -0300

    correct test errors

--- src/TestSensor.java
+++ src/TestSensor.java
@@ -1,5 +1,7 @@
 import eta.TestETA;
 
+import javax.management.InvalidAttributeValueException;
+
 public class TestSensor extends TestETA {
     Sensor sensor;
 
@@ -14,35 +16,78 @@ public class TestSensor extends TestETA {
     }
 
     public void testNivelAtualAgua(){
-        sensor.setNivelAgua(19);
-        assertEquals(sensor.getNivelAgua(), 19);
+        try {
+            sensor.setNivelAgua(19);
+            assertEquals(19, sensor.getNivelAgua());
+        }catch (InvalidAttributeValueException e){
+            fail("Failed");
+        }
+    }
+
+    public void testNivelAtualAguaAbaixo0(){
+        try{
+            sensor.setNivelAgua(-1);
+            fail("Failed");
+        }catch (InvalidAttributeValueException e){
+            sucess("Success");
+        }
+    }
+
+    public void testNivelAtualAguaAcima100(){
+        try{
+            sensor.setNivelAgua(101);
+            fail("Failed");
+        }catch (InvalidAttributeValueException e){
+            sucess("Success");
+        }
     }
 
     public void testSensorAcima0Abaixo20(){
-        sensor.setNivelAgua(19);
-        assertEquals(sensor.getAltoFalante(), "ligado");
-        assertEquals(sensor.getAlarmeVisual(), "ligado");
-        assertEquals(sensor.getParar(), false);
+        try{
+            sensor.setNivelAgua(19);
+            assertEquals("ligado", sensor.getAltoFalante());
+            assertEquals("ligado", sensor.getAlarmeVisual());
+            assertEquals("false", sensor.getParar());
+        }catch (InvalidAttributeValueException e){
+            fail("Failed");
+        }
     }
 
     public void testSensorAcima19Abaixo50(){
-        sensor.setNivelAgua(20);
-        assertEquals(sensor.getAlarmeVisual(), "piscando");
-        assertEquals(sensor.getAltoFalante(), "desligado");
-        assertEquals(sensor.getParar(), false);
+        try {
+            sensor.setNivelAgua(20);
+            assertEquals("piscando", sensor.getAlarmeVisual());
+            assertEquals("desligado", sensor.getAltoFalante());
+            assertEquals("false", sensor.getParar());
+        }catch (InvalidAttributeValueException e){
+            fail("Failed");
+        }
     }
 
     public void testSensorAcima49Abaixo100(){
-        sensor.setNivelAgua(50);
-        assertEquals(sensor.getAltoFalante(), "desligado");
-        assertEquals(sensor.getAlarmeVisual(), "desligado");
-        assertEquals(sensor.getParar(), false);
+        try{
+            sensor.setNivelAgua(50);
+            assertEquals("desligado", sensor.getAltoFalante());
+            assertEquals("desligado", sensor.getAlarmeVisual());
+            assertEquals("false", sensor.getParar());
+        }catch (InvalidAttributeValueException e){
+            fail("Failed");
+        }
     }
 
     public void testSensor100(){
-        sensor.setNivelAgua(100);
-        assertEquals(sensor.getAltoFalante(), "desligado");
-        assertEquals(sensor.getAlarmeVisual(), "desligado");
-        assertEquals(sensor.getParar(), true);
+        try{
+            sensor.setNivelAgua(100);
+            assertEquals("desligado", sensor.getAltoFalante());
+            assertEquals("desligado", sensor.getAlarmeVisual());
+            assertEquals("true", sensor.getParar());
+        }catch (InvalidAttributeValueException e){
+            fail("Failed");
+        }
+    }
+
+    public static void main(String[] args) {
+        TestSensor testSensor = new TestSensor();
+        testSensor.run();
     }
 }
